04f7e12847a67a6d51999bdb6480ae2008adb926
JesusUrban <142059202+JesusUrban@users.noreply.github.com>
2025-02-09 14:27:08 -0500

    third-commit

--- src/pokemon/Pokemon.jsx
+++ src/pokemon/Pokemon.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import './styles/Pokemon.scss';
+
 
 const POKEAPI_Base = 'https://pokeapi.co/api/v2'
 
@@ -25,33 +27,45 @@ function Pokemon() {
     const ability = pokemon?.abilities?.map(a => a.ability.name)
      
     const [hp, attack, defense, specialAttack, specialDefense, speed] = pokemon?.stats || []
+   
+   
+   
+   
     return (
-    <div>
+      <div className="pokemon-container">
         
-  <img src={pokemon?.sprites?.other['official-artwork']?.front_default} alt={pokemon?.name} />
-  <span>ID: #{pokemon?.id?.toString().padStart(3,0)}</span>
-  <h2>{pokemon?.name}</h2>
-  <p>Weight: {pokemon?.weight}</p>
-  <p>Height: {pokemon?.height}</p>
-  <p>Types: {types?.join(', ')}</p>
-  <p>Abilities: {ability?.join(', ')}</p>
-
-
-  <p>{hp?.stat.name}   <span> {hp?.base_stat} </span></p>
-  <p>{attack?.stat.name}   <span> {attack?.base_stat} </span></p>
-  <p>{defense?.stat.name}   <span> {defense?.base_stat} </span></p>
-  <p>{specialAttack?.stat.name}   <span> {specialAttack?.base_stat} </span></p>
-  <p>{specialDefense?.stat.name}   <span> {specialDefense?.base_stat} </span></p>
-  <p>{speed?.stat.name}   <span> {speed?.base_stat} </span></p>
+        <img
+          className="pokemon-image"
+          src={pokemon?.sprites?.other['official-artwork']?.front_default}
+          alt={pokemon?.name}
+        />
+        <span className="pokemon-id">ID: #{pokemon?.id?.toString().padStart(3, 0)}</span>
+        <h2 className="pokemon-name">{pokemon?.name}</h2>
+        <p className="pokemon-types">Types: <span>{types?.join(', ')}</span></p>
+        <p className="pokemon-abilities">Abilities: <span>{ability?.join(', ')}</span></p>
 
 
 
-<ul>
- {pokemon?.moves?.map(m => (<li key={m.move.name}>{m.move.name}</li>))}
-  
-</ul>
-    </div>
-  )
+        <div className="pokemon__stats ">
+          <p>{hp?.stat.name}: <span>{hp?.base_stat}</span></p>
+          <p>{attack?.stat.name}: <span>{attack?.base_stat}</span></p>
+          <p>{defense?.stat.name}: <span>{defense?.base_stat}</span></p>
+          <p>{specialAttack?.stat.name}: <span>{specialAttack?.base_stat}</span></p>
+          <p>{specialDefense?.stat.name}: <span>{specialDefense?.base_stat}</span></p>
+          <p>{speed?.stat.name}: <span>{speed?.base_stat}</span></p>
+
+
+        </div>
+
+        <ul className="pokemon-moves">
+          {pokemon?.moves?.map(m => (
+            <li key={m.move.name}>{m.move.name}</li>
+          ))}
+        </ul>
+        
+      </div>
+    )
+    
 }
 
 export default Pokemon
